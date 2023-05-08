65f612793a1c60e8ecc728dd4ae9782891db17e8
aarriiaan <aarriiaann@gmail.com>
2023-05-08 23:03:16 +0200

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     <div className="w-full min-h-screen flex flex-col items-center gap-8">
       {/* @ts-expect-error Server Component */}
       <ProfileInfo />
-      <Tabs projects={data.projects} skills={data.skills} />
+      {/*<Tabs projects={data.projects} skills={data.skills} />*/}
       <Footer />
     </div>
   );
