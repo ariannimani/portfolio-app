1df1c2e89d0a1991a74b11c5d903694fd5cd5e22
aarriiaan <aarriiaann@gmail.com>
2023-05-08 22:27:07 +0200

    update main page component

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import { getDataFromFirebase } from "../firebase/getData";
 export default async function Home() {
   const data = await getDataFromFirebase();
 
-  if (!data) return <></>;
-  const { projects, skills } = data;
+  //if (!data) return <></>;
+  //const { projects, skills } = data;
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center gap-8">
       {/* @ts-expect-error Server Component */}
       <ProfileInfo />
-      <Tabs projects={projects} skills={skills} />
+      <Tabs projects={data.projects} skills={data.skills} />
       <Footer />
     </div>
   );
